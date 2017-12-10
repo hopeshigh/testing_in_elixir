28b02b3a2024447672af87d5be6bc2ab456c0b58
danielschambers <daniel.s.chambers@gmail.com>
2017-12-10 15:59:23 +0000

    adds other list bits

--- chapter_2/test/chapter_2/pattern_lists_test.exs
+++ chapter_2/test/chapter_2/pattern_lists_test.exs
@@ -1,10 +1,50 @@
 defmodule Chapter2.PatternListsTest do
   use ExUnit.Case
+  doctest Chapter2.PatternLists
+  
   alias Chapter2.PatternLists
 
-  describe "add/2" do
-    test "adds two numbers together" do
-  
+  describe "add_item/2" do
+    test "adds input to an empty list" do
+      actual = PatternLists.add_item(2)
+      expected = [2]
+
+      assert ^expected = actual
+    end
+
+    test "adds input to a populated list" do
+      actual = PatternLists.add_item([2], 2)
+      expected = [2, 2]
+
+      assert ^expected = actual
+    end
+
+    test "adds a list to an empty list" do
+      actual = PatternLists.add_item([], [2])
+      expected = [2]
+
+      assert ^expected = actual
+    end
+
+    test "adds a list to a populated list" do
+      actual = PatternLists.add_item([123], [456])
+      expected = [123, 456]
+
+      assert ^expected = actual
+    end
+
+    test "combines two inputs of same type into a list" do
+      actual = PatternLists.add_item("Hello", "World")
+      expected = ["Hello", "World"]
+
+      assert ^expected = actual
+    end
+
+    test "combines two inputs of different types into a list" do
+      actual = PatternLists.add_item("Hello", %{hello: "Dave"})
+      expected = ["Hello", %{hello: "Dave"}]
+
+      assert ^expected = actual
     end
   end
 end
--- chapter_2/test/chapter_2/troublesome_lists_test.exs
+++ chapter_2/test/chapter_2/troublesome_lists_test.exs
@@ -1,8 +1,33 @@
 defmodule Chapter2.TroublesomeListsTest do
   use ExUnit.Case
+  doctest Chapter2.TroublesomeLists
+  
   alias Chapter2.TroublesomeLists
 
   describe "add_item/2" do
+    test "adds charlist to an empty list" do
+      actual = TroublesomeLists.add_item('hello')
+      expected = ['hello']
 
+      assert actual == expected
+    end
+
+    test "adds charlist to a list" do
+      actual = TroublesomeLists.add_item('hello', 'dave')
+      expected = [104, 101, 108, 108, 111, 100, 97, 118, 101]
+
+      assert actual == expected
+    end
+
+    test "a list of numbers returns a phrase" do
+      actual = TroublesomeLists.add_item(
+        [104, 101, 108, 108, 111], 
+        [100, 97, 118, 101]
+      )
+
+      expected = 'hellodave'
+
+      assert actual == expected
+    end
   end
 end
